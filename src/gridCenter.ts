f2e8e2d694a5b008ead66311cddb48ab2b0755f5
dance2die <sungmkim80@gmail.com>
2021-06-13 13:11:34 -0400

    smaller but less readable implementation...

--- src/gridCenter.ts
+++ src/gridCenter.ts
@@ -1,24 +1,31 @@
 const plugin = require('tailwindcss/plugin')
 
 function gridCenter({ addUtilities }: { addUtilities: any }) {
-  const utilities = {
-    '.grid-center': {
-      display: 'grid',
-      'place-items': 'center',
-    },
-    '.grid-start': {
-      display: 'grid',
-      'place-items': 'start',
-    },
-    '.grid-end': {
-      display: 'grid',
-      'place-items': 'end',
-    },
-    '.grid-stretch': {
-      display: 'grid',
-      'place-items': 'stretch',
-    },
-  }
+  const placementList = ['center', 'start', 'end', 'stretch'];
+  const utilities = Object.fromEntries(
+    placementList.map(placement => ([
+      `.grid-${placement}`, { display: 'grid', 'place-items': placement }
+    ]))
+  )
+
+  // const utilities = {
+  //   '.grid-center': {
+  //     display: 'grid',
+  //     'place-items': 'center',
+  //   },
+  //   '.grid-start': {
+  //     display: 'grid',
+  //     'place-items': 'start',
+  //   },
+  //   '.grid-end': {
+  //     display: 'grid',
+  //     'place-items': 'end',
+  //   },
+  //   '.grid-stretch': {
+  //     display: 'grid',
+  //     'place-items': 'stretch',
+  //   },
+  // }
   // Same as 'place-items': https://tailwindcss.com/docs/place-items#variants
   const variants = ['responsive']
   addUtilities(utilities, { variants })
