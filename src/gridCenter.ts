47c47acabff92ed6db263378f21d3c16b46baaad
dance2die <sungmkim80@gmail.com>
2021-06-13 13:13:52 -0400

    changed back to readable code

--- src/gridCenter.ts
+++ src/gridCenter.ts
@@ -1,13 +1,24 @@
 const plugin = require('tailwindcss/plugin')
 
 function gridCenter({ addUtilities }: { addUtilities: any }) {
-  const placementList = ['center', 'start', 'end', 'stretch'];
-  const utilities = Object.fromEntries(
-    placementList.map(placement => ([
-      `.grid-${placement}`, { display: 'grid', 'place-items': placement }
-    ]))
-  )
-
+  const utilities = {
+    '.grid-center': {
+      display: 'grid',
+      'place-items': 'center',
+    },
+    '.grid-start': {
+      display: 'grid',
+      'place-items': 'start',
+    },
+    '.grid-end': {
+      display: 'grid',
+      'place-items': 'end',
+    },
+    '.grid-stretch': {
+      display: 'grid',
+      'place-items': 'stretch',
+    },
+  }
   // Same as 'place-items': https://tailwindcss.com/docs/place-items#variants
   const variants = ['responsive']
   addUtilities(utilities, { variants })
