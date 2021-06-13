99c494679d99d392307d72d22218d84094460177
dance2die <sungmkim80@gmail.com>
2021-06-13 13:12:33 -0400

    removed old code

--- src/gridCenter.ts
+++ src/gridCenter.ts
@@ -8,24 +8,6 @@ function gridCenter({ addUtilities }: { addUtilities: any }) {
     ]))
   )
 
-  // const utilities = {
-  //   '.grid-center': {
-  //     display: 'grid',
-  //     'place-items': 'center',
-  //   },
-  //   '.grid-start': {
-  //     display: 'grid',
-  //     'place-items': 'start',
-  //   },
-  //   '.grid-end': {
-  //     display: 'grid',
-  //     'place-items': 'end',
-  //   },
-  //   '.grid-stretch': {
-  //     display: 'grid',
-  //     'place-items': 'stretch',
-  //   },
-  // }
   // Same as 'place-items': https://tailwindcss.com/docs/place-items#variants
   const variants = ['responsive']
   addUtilities(utilities, { variants })
